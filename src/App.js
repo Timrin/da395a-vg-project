b4ed8795d9676949c21b5d2ca591db74d86fb422
Timrin <19280360+Timrin@users.noreply.github.com>
2022-08-25 11:23:30 +0200

    Implement and add some icons for the sections

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ function App() {
       <div id='Header' className='section'></div>
 
       <div id='Section-Filter' className='section'>
+        <div className="sectionIcon">
+          <span className="material-icons-outlined">filter_list</span>
+        </div>
         <h1>Active Filters</h1>
       </div>
 
       <div id='Section-Suggestion' className='section'>
+        <div className="sectionIcon">
+          <span className="material-icons-outlined">view_carousel</span>
+        </div>
         <h1>Suggestions</h1>
       </div>
-      
+
       <div id='Section-Pinned' className='section'>
+        <div className="sectionIcon">
+          <span className="material-icons-outlined">push_pin</span>
+        </div>
         <h1>Pinned</h1>
       </div>
     </div>
