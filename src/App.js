ad326a90834573d6a46f718be701903a70db456e
Timrin <19280360+Timrin@users.noreply.github.com>
2022-08-25 10:55:07 +0200

    Start building the site. Add the basic sections

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div id='Header' className='section'></div>
+
+      <div id='Section-Filter' className='section'>
+        <h1>Active Filters</h1>
+      </div>
+
+      <div id='Section-Suggestion' className='section'>
+        <h1>Suggestions</h1>
+      </div>
+      
+      <div id='Section-Pinned' className='section'>
+        <h1>Pinned</h1>
+      </div>
     </div>
   );
 }
