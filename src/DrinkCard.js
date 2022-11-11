de1145d8d01724d7a33d5d2a0ad081a2912b7fc2
Timothy Timrin <19280360+Timrin@users.noreply.github.com>
2022-11-11 13:46:31 +0100

    Update drink card with accordions
    Redesign the drink card component with Accordions

--- src/DrinkCard.js
+++ src/DrinkCard.js
@@ -7,27 +7,42 @@ function DrinkCard(props) {
     return (
         <Card style={{ height: "600px", overflow: "scroll", borderRadius: "20px", border: "none", borderRadius: "20px", boxShadow: "0px 4px 20px 0px rgb(0 0 0 / 50%)", textAlign: "left" }}>
 
-            <Card.Img variant="top" src={drink.imageLink} style={{ height: "250px", objectFit: "cover", objectPosition: "top" }} />
+                                    <Card.Img variant="top" src={drink.imageLink} style={{ height: "250px", objectFit: "cover", objectPosition: "top" }} />
 
-            <Card.Body style={{ flex: "inherit" }}>
-                <Card.Title>{drink.name}</Card.Title>
-                <Card.Text>{drink.alcoholic} • {drink.type}</Card.Text>
-            </Card.Body>
+                                    <Card.Body style={{ flex: "inherit" }}>
+                                        <Card.Title>{drink.name}</Card.Title>
+                                        <Card.Text>{drink.alcoholic} • {drink.type}</Card.Text>
+                                    </Card.Body>
 
-            <ListGroup variant="flush">
-                {
-                    drink.ingredients.map((ingredient) => {
-                        return (<ListGroup.Item>{ingredient.amount} {ingredient.name}</ListGroup.Item>);
-                    })
-                }
-            </ListGroup>
+                                    <Accordion flush style={{ borderTop: "1px solid #dee2e6", borderBottom: "1px solid #dee2e6" }}>
+                                        <Accordion.Item eventKey="0">
+                                            <Accordion.Header>
+                                                <span class="material-symbols-outlined" style={{ paddingRight: "var(--bs-accordion-btn-padding-x)" }}>liquor</span>
+                                                Ingredients
+                                            </Accordion.Header>
+                                            <Accordion.Body>
+                                                <ListGroup variant="flush">
+                                                    {
+                                                        drink.ingredients.map((ingredient) => {
+                                                            return (<ListGroup.Item>{ingredient.amount} {ingredient.name}</ListGroup.Item>);
+                                                        })
+                                                    }
+                                                </ListGroup>
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                        <Accordion.Item eventKey="1">
+                                            <Accordion.Header>
+                                                <span class="material-symbols-outlined" style={{ paddingRight: "var(--bs-accordion-btn-padding-x)" }}>menu_book</span>
+                                                Instructions
+                                            </Accordion.Header>
+                                            <Accordion.Body>
+                                                {drink.instructions}
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                    </Accordion>
 
-            <Card.Body>
-                {drink.instructions}
-            </Card.Body>
-
-        </Card>
+                                </Card>
     );
-}
-
-export default DrinkCard;
+  }
+  
+  export default DrinkCard;
