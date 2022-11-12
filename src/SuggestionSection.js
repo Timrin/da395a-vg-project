629737423500abf2d95e76973cbafffe38638618
Timothy Timrin <19280360+Timrin@users.noreply.github.com>
2022-11-12 14:40:33 +0100

    Added a button to refresh the drink suggestions

--- src/SuggestionSection.js
+++ src/SuggestionSection.js
@@ -90,7 +90,7 @@ function SuggestionSection(props) {
 
     const [drinks, setDrinks] = useState([]);
 
-    useEffect(()=>{getNewSuggestions(setDrinks)}, [])
+    useEffect(() => { getNewSuggestions(setDrinks) }, [])
 
     return (
         <div id='Section-Suggestion' className='section'>
@@ -98,9 +98,9 @@ function SuggestionSection(props) {
             <SectionHeader sectionTitle="Suggestions" sectionIcon="view_carousel"></SectionHeader>
 
             <div className='sectionBody container'>
-                <div className="suggestionWrapper" style={{margin: "30px 0px"}}>
+                <div className="suggestionWrapper" style={{ margin: "30px 0px" }}>
                     {
-                        
+
                         drinks?.map((e) => {
                             console.log(drinks);
                             return (
@@ -109,6 +109,22 @@ function SuggestionSection(props) {
                         })
                     }
                 </div>
+
+                <Button onClick={() => { getNewSuggestions(setDrinks) }} style={{
+                    display: "flex",
+                    color: "#fff",
+                    fontWeight: "600",
+                    backgroundColor: "#000",
+                    border: "none",
+                    borderRadius: "100px",
+                    padding: "1rem 1.5rem",
+                    margin: "auto",
+                    marginBottom: "30px"
+                }}>
+                    New Suggestions
+                    <span class="material-symbols-outlined" style={{paddingLeft: "3px"}}>refresh</span>
+                </Button>
+
             </div>
 
         </div>
